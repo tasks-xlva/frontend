94f85aeaa6fd329c4160fd61dc23c2451b2df554
straivi <bme15122000@gmail.com>
2021-10-24 00:50:12 +0300

    update public layout

---
+++ src/pages/public-layout/public-layout.tsx
@@ -0,0 +1,38 @@
+import { Drawer, PageHeader } from 'antd'
+import { ReactNode, useState } from 'react'
+import styles from './public-layout.module.scss'
+import { MenuOutlined } from '@ant-design/icons'
+import { useVisible } from '../../hooks/use-viisible'
+
+interface Props {
+  children: ReactNode
+}
+
+export const PublicLayout = (props: Props) => {
+  const { isVisible, show, hide } = useVisible()
+  const { children } = props
+
+  return (
+    <>
+      <PageHeader
+        backIcon={<MenuOutlined />}
+        onBack={show}
+        title='Public title'
+        subTitle='This is a subtitle'
+      />
+      <main className={styles.wrapper}>{children}</main>
+      <Drawer
+        title='Basic Drawer'
+        placement='left'
+        width='100%'
+        className={styles.drawer}
+        visible={isVisible}
+        onClose={hide}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
+  )
+}
