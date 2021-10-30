2c6e642292e9bcbefa953cd81a81b62af954ca52
straivi <bme15122000@gmail.com>
2021-10-31 00:46:52 +0300

    [style] add style to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'antd/dist/antd.variable.min.css'
 import 'src/styles/colors.scss'
 import { ConfigProvider } from 'antd'
+import './styles/index.scss'
 
 ConfigProvider.config({ theme: { primaryColor: '#272727' } })
 
