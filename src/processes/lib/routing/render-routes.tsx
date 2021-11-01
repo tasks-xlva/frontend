1f64a291345b6122ae6a02460af9c53e2eecebec
xalvaine <xalvaine@gmail.com>
2021-11-02 00:20:01 +0300

    [refactor] Add skipped files to git

---
+++ src/processes/lib/routing/render-routes.tsx
@@ -0,0 +1,18 @@
+import { Redirect, Route, Switch } from 'react-router-dom'
+import { Routes } from './routes'
+
+export const renderRoutes = (routes: Routes) => {
+  const defaultPath = routes.find((route) => route.default)?.path
+
+  return (
+    <Switch>
+      <Redirect from='/:url*(/+)' to={window.location.pathname.slice(0, -1)} />
+      {routes.map((route) => (
+        <Route exact key={route.path} path={route.path}>
+          <route.component />
+        </Route>
+      ))}
+      {defaultPath && <Redirect to={defaultPath} />}
+    </Switch>
+  )
+}
