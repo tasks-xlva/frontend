37d6a8c2f18cd38e3aa5a90e28a91913938627c7
straivi <bme15122000@gmail.com>
2021-11-13 21:15:04 +0300

    [feat] Create swr config

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import 'antd/dist/antd.variable.min.css'
 import { App } from 'app'
 import { ConfigProvider } from 'antd'
 import { Authorization } from 'features/lib/authorization'
+import { api } from 'shared/api'
+import { SWRConfig } from 'swr'
+import { AxiosRequestConfig } from 'axios'
 
 ConfigProvider.config({ theme: { primaryColor: '#272727' } })
 
@@ -12,7 +15,14 @@ ReactDOM.render(
   <Authorization>
     <ConfigProvider componentSize='large'>
       <BrowserRouter>
-        <App />
+        <SWRConfig
+          value={{
+            fetcher: (url: string, config?: AxiosRequestConfig) =>
+              api.get(url, config).then((response) => response.data),
+          }}
+        >
+          <App />
+        </SWRConfig>
       </BrowserRouter>
     </ConfigProvider>
   </Authorization>,
