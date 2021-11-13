f9d0816135967ce5af2475bc6980b9d1fa0ac3e8
Timofey <xalvaine@yandex-team.ru>
2021-11-14 00:14:50 +0300

    Dtsgenerator (#20)
    * [chore] Update schema path
    
    * [refactor] Update types
    
    * [fix] Fix ts errors

--- src/entities/api/groups.ts
+++ src/entities/api/groups.ts
@@ -2,6 +2,6 @@ import useSWR from 'swr'
 import { routes } from 'shared/api'
 
 export const useGroups = () => {
-  const { data: groups } = useSWR<Definitions.Group[]>(routes.groups)
+  const { data: groups } = useSWR<Components.Schemas.Group[]>(routes.groups)
   return { groups }
 }
--- src/shared/types/gen/backend.d.ts
+++ src/shared/types/gen/backend.d.ts
@@ -1,366 +1,485 @@
-declare namespace Definitions {
-  export interface Group {
-    /**
-     * ID
-     */
-    id: number
-    /**
-     * Number
-     */
-    number: string
-    subjects?: Subject[]
-  }
-  export interface MyPassword {
-    /**
-     * Password
-     */
-    password: string
-  }
-  export interface MyProfile {
-    /**
-     * First name
-     */
-    firstName?: string
-    /**
-     * Last name
-     */
-    lastName?: string
-    /**
-     * Почта
-     */
-    email: string // email
-  }
-  export interface Subject {
-    /**
-     * ID
-     */
-    id?: number
-    /**
-     * Name
-     */
-    name: string
-    /**
-     * Description
-     */
-    description?: string
-    tasks?: Task[]
-  }
-  export interface Task {
-    /**
-     * Name
-     */
-    name: string
-    /**
-     * Description
-     */
-    description: string
-    /**
-     * Subject
-     */
-    subject: string // uri
-  }
-  export interface TokenObtainPair {
-    /**
-     * Email
-     */
-    email: string
-    /**
-     * Password
-     */
-    password: string
-  }
-  export interface TokenObtainPairResponse {
-    /**
-     * Access
-     */
-    access: string
-    /**
-     * Refresh
-     */
-    refresh: string
-  }
-  export interface TokenRefresh {
-    /**
-     * Refresh
-     */
-    refresh: string
-    /**
-     * Access
-     */
-    access?: string
-  }
-  export interface TokenRefreshResponse {
-    /**
-     * Access
-     */
-    access: string
-    /**
-     * Refresh
-     */
-    refresh: string
-  }
-  export interface User {
-    /**
-     * First name
-     */
-    firstName?: string
-    /**
-     * Last name
-     */
-    lastName?: string
-    /**
-     * Почта
-     */
-    email: string // email
-    /**
-     * Password
-     */
-    password: string
+declare namespace Components {
+  namespace Schemas {
+    export interface Group {
+      id: number
+      number: string
+      subjects: Subject[]
+    }
+    export interface GroupRequest {
+      number: string
+    }
+    export interface MyPassword {
+      /**
+       * Пароль
+       */
+      password: string
+    }
+    export interface MyPasswordRequest {
+      /**
+       * Пароль
+       */
+      password: string
+    }
+    export interface MyProfile {
+      id: number
+      /**
+       * Имя
+       */
+      first_name?: string
+      /**
+       * Фамилия
+       */
+      last_name?: string
+      /**
+       * Почта
+       */
+      email: string // email
+    }
+    export interface MyProfileRequest {
+      /**
+       * Имя
+       */
+      first_name?: string
+      /**
+       * Фамилия
+       */
+      last_name?: string
+      /**
+       * Почта
+       */
+      email: string // email
+    }
+    export interface PatchedGroupRequest {
+      number?: string
+    }
+    export interface PatchedMyPasswordRequest {
+      /**
+       * Пароль
+       */
+      password?: string
+    }
+    export interface PatchedMyProfileRequest {
+      /**
+       * Имя
+       */
+      first_name?: string
+      /**
+       * Фамилия
+       */
+      last_name?: string
+      /**
+       * Почта
+       */
+      email?: string // email
+    }
+    export interface PatchedSubjectRequest {
+      name?: string
+      description?: string | null
+      group_id?: number
+    }
+    export interface PatchedTaskRequest {
+      name?: string
+      description?: string
+      subject?: string // uri
+    }
+    export interface Subject {
+      id: number
+      name: string
+      description?: string | null
+      tasks: Task[]
+      group_id: number
+    }
+    export interface SubjectRequest {
+      name: string
+      description?: string | null
+      group_id: number
+    }
+    export interface Task {
+      id: number
+      name: string
+      description: string
+      subject: string // uri
+    }
+    export interface TaskRequest {
+      name: string
+      description: string
+      subject: string // uri
+    }
+    export interface TokenObtainPair {
+      access: string
+      refresh: string
+    }
+    export interface TokenObtainPairRequest {
+      email: string
+      password: string
+    }
+    export interface TokenRefresh {
+      access: string
+      refresh: string
+    }
+    export interface TokenRefreshRequest {
+      refresh: string
+    }
+    export interface User {
+      id: number
+      /**
+       * Имя
+       */
+      first_name?: string
+      /**
+       * Фамилия
+       */
+      last_name?: string
+      /**
+       * Почта
+       */
+      email: string // email
+    }
+    export interface UserRequest {
+      /**
+       * Имя
+       */
+      first_name?: string
+      /**
+       * Фамилия
+       */
+      last_name?: string
+      /**
+       * Почта
+       */
+      email: string // email
+      password: string
+    }
   }
 }
 declare namespace Paths {
-  namespace Groups$Id {
+  namespace GroupsCreate {
+    export type RequestBody = Components.Schemas.GroupRequest
+    namespace Responses {
+      export type $201 = Components.Schemas.Group
+    }
+  }
+  namespace GroupsDestroy {
     namespace Parameters {
-      /**
-       * A unique integer value identifying this group.
-       */
       export type Id = number
     }
     export interface PathParameters {
-      id: /* A unique integer value identifying this group. */ Parameters.Id
-    }
-  }
-  namespace GroupsCreate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.Group
+      id: Parameters.Id
     }
     namespace Responses {
-      export type $201 = Definitions.Group
+      export interface $204 {}
     }
   }
   namespace GroupsList {
     namespace Responses {
-      export type $200 = Definitions.Group[]
+      export type $200 = Components.Schemas.Group[]
     }
   }
   namespace GroupsPartialUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
     namespace Parameters {
-      export type Data = Definitions.Group
-    }
-    namespace Responses {
-      export type $200 = Definitions.Group
-    }
-  }
-  namespace GroupsRead {
-    namespace Responses {
-      export type $200 = Definitions.Group
-    }
-  }
-  namespace GroupsUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.Group
-    }
-    namespace Responses {
-      export type $200 = Definitions.Group
-    }
-  }
-  namespace Subjects$Id {
-    namespace Parameters {
-      /**
-       * A unique integer value identifying this subject.
-       */
       export type Id = number
     }
     export interface PathParameters {
-      id: /* A unique integer value identifying this subject. */ Parameters.Id
+      id: Parameters.Id
+    }
+    export type RequestBody = Components.Schemas.PatchedGroupRequest
+    namespace Responses {
+      export type $200 = Components.Schemas.Group
+    }
+  }
+  namespace GroupsRetrieve {
+    namespace Parameters {
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.Group
+    }
+  }
+  namespace GroupsUpdate {
+    namespace Parameters {
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
+    export type RequestBody = Components.Schemas.GroupRequest
+    namespace Responses {
+      export type $200 = Components.Schemas.Group
+    }
+  }
+  namespace SchemaRetrieve {
+    namespace Parameters {
+      export type Format = 'json' | 'yaml'
+      export type Lang =
+        | 'af'
+        | 'ar'
+        | 'ar-dz'
+        | 'ast'
+        | 'az'
+        | 'be'
+        | 'bg'
+        | 'bn'
+        | 'br'
+        | 'bs'
+        | 'ca'
+        | 'cs'
+        | 'cy'
+        | 'da'
+        | 'de'
+        | 'dsb'
+        | 'el'
+        | 'en'
+        | 'en-au'
+        | 'en-gb'
+        | 'eo'
+        | 'es'
+        | 'es-ar'
+        | 'es-co'
+        | 'es-mx'
+        | 'es-ni'
+        | 'es-ve'
+        | 'et'
+        | 'eu'
+        | 'fa'
+        | 'fi'
+        | 'fr'
+        | 'fy'
+        | 'ga'
+        | 'gd'
+        | 'gl'
+        | 'he'
+        | 'hi'
+        | 'hr'
+        | 'hsb'
+        | 'hu'
+        | 'hy'
+        | 'ia'
+        | 'id'
+        | 'ig'
+        | 'io'
+        | 'is'
+        | 'it'
+        | 'ja'
+        | 'ka'
+        | 'kab'
+        | 'kk'
+        | 'km'
+        | 'kn'
+        | 'ko'
+        | 'ky'
+        | 'lb'
+        | 'lt'
+        | 'lv'
+        | 'mk'
+        | 'ml'
+        | 'mn'
+        | 'mr'
+        | 'my'
+        | 'nb'
+        | 'ne'
+        | 'nl'
+        | 'nn'
+        | 'os'
+        | 'pa'
+        | 'pl'
+        | 'pt'
+        | 'pt-br'
+        | 'ro'
+        | 'ru'
+        | 'sk'
+        | 'sl'
+        | 'sq'
+        | 'sr'
+        | 'sr-latn'
+        | 'sv'
+        | 'sw'
+        | 'ta'
+        | 'te'
+        | 'tg'
+        | 'th'
+        | 'tk'
+        | 'tr'
+        | 'tt'
+        | 'udm'
+        | 'uk'
+        | 'ur'
+        | 'uz'
+        | 'vi'
+        | 'zh-hans'
+        | 'zh-hant'
+    }
+    export interface QueryParameters {
+      format?: Parameters.Format
+      lang?: Parameters.Lang
+    }
+    namespace Responses {
+      export interface $200 {
+        [name: string]: any
+      }
     }
   }
   namespace SubjectsCreate {
-    export interface BodyParameters {
-      data: Parameters.Data
+    export type RequestBody = Components.Schemas.SubjectRequest
+    namespace Responses {
+      export type $201 = Components.Schemas.Subject
     }
+  }
+  namespace SubjectsDestroy {
     namespace Parameters {
-      export type Data = Definitions.Subject
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
     }
     namespace Responses {
-      export type $201 = Definitions.Subject
+      export interface $204 {}
     }
   }
   namespace SubjectsList {
     namespace Responses {
-      export type $200 = Definitions.Subject[]
+      export type $200 = Components.Schemas.Subject[]
     }
   }
   namespace SubjectsPartialUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
     namespace Parameters {
-      export type Data = Definitions.Subject
-    }
-    namespace Responses {
-      export type $200 = Definitions.Subject
-    }
-  }
-  namespace SubjectsRead {
-    namespace Responses {
-      export type $200 = Definitions.Subject
-    }
-  }
-  namespace SubjectsUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.Subject
-    }
-    namespace Responses {
-      export type $200 = Definitions.Subject
-    }
-  }
-  namespace Tasks$Id {
-    namespace Parameters {
-      /**
-       * A unique integer value identifying this task.
-       */
       export type Id = number
     }
     export interface PathParameters {
-      id: /* A unique integer value identifying this task. */ Parameters.Id
+      id: Parameters.Id
+    }
+    export type RequestBody = Components.Schemas.PatchedSubjectRequest
+    namespace Responses {
+      export type $200 = Components.Schemas.Subject
+    }
+  }
+  namespace SubjectsRetrieve {
+    namespace Parameters {
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.Subject
+    }
+  }
+  namespace SubjectsUpdate {
+    namespace Parameters {
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
+    export type RequestBody = Components.Schemas.SubjectRequest
+    namespace Responses {
+      export type $200 = Components.Schemas.Subject
     }
   }
   namespace TasksCreate {
-    export interface BodyParameters {
-      data: Parameters.Data
+    export type RequestBody = Components.Schemas.TaskRequest
+    namespace Responses {
+      export type $201 = Components.Schemas.Task
     }
+  }
+  namespace TasksDestroy {
     namespace Parameters {
-      export type Data = Definitions.Task
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
     }
     namespace Responses {
-      export type $201 = Definitions.Task
+      export interface $204 {}
     }
   }
   namespace TasksList {
     namespace Responses {
-      export type $200 = Definitions.Task[]
+      export type $200 = Components.Schemas.Task[]
     }
   }
   namespace TasksPartialUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
     namespace Parameters {
-      export type Data = Definitions.Task
+      export type Id = number
     }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
+    export type RequestBody = Components.Schemas.PatchedTaskRequest
     namespace Responses {
-      export type $200 = Definitions.Task
+      export type $200 = Components.Schemas.Task
     }
   }
-  namespace TasksRead {
+  namespace TasksRetrieve {
+    namespace Parameters {
+      export type Id = number
+    }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
     namespace Responses {
-      export type $200 = Definitions.Task
+      export type $200 = Components.Schemas.Task
     }
   }
   namespace TasksUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
     namespace Parameters {
-      export type Data = Definitions.Task
+      export type Id = number
     }
+    export interface PathParameters {
+      id: Parameters.Id
+    }
+    export type RequestBody = Components.Schemas.TaskRequest
     namespace Responses {
-      export type $200 = Definitions.Task
+      export type $200 = Components.Schemas.Task
     }
   }
   namespace TokenCreate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.TokenObtainPair
-    }
+    export type RequestBody = Components.Schemas.TokenObtainPairRequest
     namespace Responses {
-      export type $200 = Definitions.TokenObtainPairResponse
+      export type $200 = Components.Schemas.TokenObtainPair
     }
   }
   namespace TokenRefreshCreate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.TokenRefresh
-    }
+    export type RequestBody = Components.Schemas.TokenRefreshRequest
     namespace Responses {
-      export type $200 = Definitions.TokenRefreshResponse
+      export type $200 = Components.Schemas.TokenRefresh
     }
   }
   namespace UsersCreate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.User
-    }
+    export type RequestBody = Components.Schemas.UserRequest
     namespace Responses {
-      export type $201 = Definitions.User
+      export type $201 = Components.Schemas.User
     }
   }
   namespace UsersMePartialUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.MyProfile
-    }
+    export type RequestBody = Components.Schemas.PatchedMyProfileRequest
     namespace Responses {
-      export type $200 = Definitions.MyProfile
+      export type $200 = Components.Schemas.MyProfile
     }
   }
   namespace UsersMePasswordPartialUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.MyPassword
-    }
+    export type RequestBody = Components.Schemas.PatchedMyPasswordRequest
     namespace Responses {
-      export type $200 = Definitions.MyPassword
+      export type $200 = Components.Schemas.MyPassword
     }
   }
   namespace UsersMePasswordUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.MyPassword
-    }
+    export type RequestBody = Components.Schemas.MyPasswordRequest
     namespace Responses {
-      export type $200 = Definitions.MyPassword
+      export type $200 = Components.Schemas.MyPassword
     }
   }
-  namespace UsersMeRead {
+  namespace UsersMeRetrieve {
     namespace Responses {
-      export type $200 = Definitions.MyProfile
+      export type $200 = Components.Schemas.MyProfile
     }
   }
   namespace UsersMeUpdate {
-    export interface BodyParameters {
-      data: Parameters.Data
-    }
-    namespace Parameters {
-      export type Data = Definitions.MyProfile
-    }
+    export type RequestBody = Components.Schemas.MyProfileRequest
     namespace Responses {
-      export type $200 = Definitions.MyProfile
+      export type $200 = Components.Schemas.MyProfile
     }
   }
 }
