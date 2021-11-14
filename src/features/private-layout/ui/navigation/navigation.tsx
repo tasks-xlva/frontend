cb2da94c412d9743f70949654b09632567df08ed
xalvaine <xalvaine@gmail.com>
2021-11-14 20:31:56 +0300

    [feat] Improve drawer ui

--- src/features/private-layout/ui/navigation/navigation.tsx
+++ src/features/private-layout/ui/navigation/navigation.tsx
@@ -2,37 +2,45 @@ import { Link } from 'react-router-dom'
 import { PRIVATE_PATH } from 'shared/config'
 import { Drawer, Typography } from 'antd'
 import { useLogout } from '../../lib/use-logout'
+import styles from './navigation.module.scss'
 
 interface Props {
   onClose: () => void
   visible: boolean
 }
 
+const pages: { link: string; title: string }[] = [
+  { title: `Мои задания`, link: PRIVATE_PATH.TASKS },
+  { title: `Предметы`, link: PRIVATE_PATH.SUBJECTS },
+  { title: `Группы`, link: PRIVATE_PATH.GROUPS },
+  { title: `Настройки`, link: PRIVATE_PATH.SETTINGS },
+]
+
 export const Navigation = ({ onClose, visible }: Props) => {
   const { handleLogout } = useLogout()
 
   return (
     <Drawer
+      className={styles.drawer}
       placement='left'
       title='Имя Фамилия'
       onClose={onClose}
       visible={visible}
+      width='100%'
     >
-      <Link to='' onClick={onClose}>
-        <p>Мои задания</p>
-      </Link>
-      <Link to={PRIVATE_PATH.SUBJECTS} onClick={onClose}>
-        <p>Предметы</p>
-      </Link>
-      <Link to={PRIVATE_PATH.GROUPS} onClick={onClose}>
-        <p>Группы</p>
-      </Link>
-      <Link to={PRIVATE_PATH.SETTINGS} onClick={onClose}>
-        <p>Настройки</p>
-      </Link>
-      <p onClick={handleLogout}>
-        <Typography.Text type='danger'>Выход</Typography.Text>
-      </p>
+      {pages.map(({ title, link }) => (
+        <Link to={link} onClick={onClose}>
+          <Typography.Title level={2}>{title}</Typography.Title>
+        </Link>
+      ))}
+      <Typography.Title
+        className={styles.logout}
+        type='danger'
+        level={3}
+        onClick={handleLogout}
+      >
+        Выход
+      </Typography.Title>
     </Drawer>
   )
 }
