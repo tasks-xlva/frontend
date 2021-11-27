0dc6e0b754e4c7d3aebeec529d0535757567b765
Timofey <xalvaine@gmail.com>
2021-11-27 20:32:34 +0300

    [feat] Add user-friendly api error handling (#55)

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,19 @@
+import { notification } from 'antd'
 import axios from 'axios'
 
 export const baseURL = `https://tasks.api.xlvn.ru/v1`
 
 export const api = axios.create({ baseURL })
+
+api.interceptors.response.use(undefined, (error) => {
+  console.log(error.response)
+  notification.error({
+    message: `Ошибка запроса`,
+    description: Object.entries(error.response.data).map(
+      ([key, value]) =>
+        `${key.charAt(0).toUpperCase()}${key.slice(1)}: ${
+          Array.isArray(value) ? value.join(`, `) : value
+        }`,
+    ),
+  })
+})
